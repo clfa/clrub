e26ec4975886428130ce88de6ea712feec2bbf56
fenggee007 <fenggee007@163.com>
2014-12-23 14:13:20 +0800

    second commit

--- clrub.sh
+++ clrub.sh
@@ -2,4 +2,13 @@
 
 #实现清除用户目录下emacs编辑留下的自动备份文件，如test.c~1等类似文件，虽然不大但很占视线
 
-find ~ -name "*~*" -exec rm {} \;
+tcp=
+if [[ -d $1 ]] && [[ -n $1 ]] ;
+then
+    tcp=$1
+else
+    tcp="/home/clf"
+fi
+# echo $tcp
+find $tcp -name "*~*" -exec rm {} \;
+
